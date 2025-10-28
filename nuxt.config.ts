14eddb2e2733f3fea26b218023fbee449e8cadfc
sonjinho <sonjinhoho@naver.com>
2025-10-28 13:56:54 +0900

    update nuxt.config.ts setting

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,6 +1,14 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  compatibilityDate: '2025-07-15',
+  compatibilityDate: "2025-07-15",
   devtools: { enabled: true },
-  modules: ['@nuxt/eslint', '@nuxt/ui']
-})
+  modules: ["@nuxt/eslint", "@nuxt/ui"],
+  ssr: false,
+  nitro: {
+    preset: "static",
+  },
+  buildDir: ".nuxt",
+  dir: {
+    public: "public",
+  },
+});
